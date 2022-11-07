b0574a73d87c38e80df9ddd9997e15ddca2caae6
Tom Hinkle <tmhinkle@gmail.com>
2022-11-07 10:51:02 -0500

    toFetch.json

--- styles/index.js
+++ styles/index.js
@@ -23,4 +23,92 @@ const styles = [
     name: "Fall Tree",
     portfolio: "",
   },
+  {
+    name: "The Cataclysm: as seen by scavenger in sewer",
+    path: "pe-ter/style.css",
+    designer: "Peter F. Parlee Carr",
+    portfolio: "",
+  },
+  {
+    name: "Radio Music",
+    path: "emily-music/style.css",
+    designer: "Emily Myerov",
+    portfolio: "",
+  },
+  {
+    name: "Halo",
+    path: "nathan-halo/style.css",
+    designer: "Nathan E.",
+    portfolio: "",
+  },
+  {
+    name: "soccer",
+    path: "emanuelly-ferreira/style.css",
+    designer: "Emanuelly Ferreira.",
+    portfolio:
+      "your personal portfolio, if you have one — or claim a URL now on netlify and build on soon :)",
+  },
+  {
+    name: "Milky Way",
+    path: "Will/style.css",
+    designer: "Will K",
+    portfolio: "",
+  },
+  {
+    name: "your design name",
+    path: "nai/style.css",
+    designer: "Naima Kaldi",
+    portfolio:
+      "your personal portfolio, if you have one — or claim a URL now on netlify and build on soon :)",
+  },
+  {
+    name: "Simple Dark",
+    path: "zander2023/style.css",
+    designer: "Zander B-V",
+    portfolio: "",
+  },
+  {
+    name: "Tri-Tec Industries",
+    path: "24-ethan-tritec/style.css",
+    designer: "Ethan-Matte",
+    portfolio: "https://sunryze-studios.netlify.app/web.html",
+  },
+  {
+    name: "Simplicity",
+    path: "24-ethan-simple/style.css",
+    designer: "Ethan-Matte",
+    portfolio: "https://sunryze-studios.netlify.app/web.html",
+  },
+  {
+    name: "Retro Game",
+    path: "24-ethan-retro/style.css",
+    designer: "Ethan-matte",
+    portfolio: "https://sunryze-studios.netlify.app/web.html",
+  },
+  {
+    name: "Zen Garden ",
+    path: "24-luis-simple/style.css",
+    designer: "Luis-Valiente",
+    portfolio:
+      "your personal portfolio, if you have one — or claim a URL now on netlify and build on soon :)",
+  },
+  {
+    name: "Construction",
+    path: "23-julian-construction/style.css",
+    designer: "julian-henry",
+    portfolio: "julianhenryportfolio.com",
+  },
+  {
+    name: "Cyberpunk",
+    path: "23-julian-cyberpunk/style.css",
+    designer: "julian-henry",
+    portfolio: "julianhenryportfolio.com",
+  },
+  {
+    name: "Haunted Graveyard",
+    path: "23-julian-haunted/style.css",
+    designer: "julian-henry",
+    portfolio: "julianhenryportfolio.com",
+  },
 ];
+exports.styles = styles;
