8e33825526f960dcba5e7f4090ee3e2cb2cc2868
Tom Hinkle <tmhinkle@gmail.com>
2023-06-05 09:31:12 -0400

    ++portfolios from S1

--- styles/index.js
+++ styles/index.js
@@ -1,182 +1,185 @@
-const styles=[
+const styles = [
   {
-    "name": "Boring Default",
-    "path": "default/style.css",
-    "designer": "Mr. Hinkle",
-    "portfolio": "https://www.tomhinkle.net",
-    "highlight": true
+    name: "Boring Default",
+    path: "default/style.css",
+    designer: "Mr. Hinkle",
+    portfolio: "https://www.tomhinkle.net",
+    highlight: true,
   },
   {
-    "name": "Golden Ratio",
-    "path": "th-fib/style.css",
-    "designer": "Mr. Hinkle",
-    "portfolio": "https://www.tomhinkle.net",
-    "highlight": true
+    name: "Golden Ratio",
+    path: "th-fib/style.css",
+    designer: "Mr. Hinkle",
+    portfolio: "https://www.tomhinkle.net",
+    highlight: true,
   },
   {
-    "name": "Block party",
-    "path": "block-party/style.css",
-    "designer": "Mr. Hinkle",
-    "portfolio": "https://www.tomhinkle.net",
-    "highlight": true
+    name: "Block party",
+    path: "block-party/style.css",
+    designer: "Mr. Hinkle",
+    portfolio: "https://www.tomhinkle.net",
+    highlight: true,
   },
   {
-    "path": "ar-tree/style.css",
-    "designer": "Aarav Patel",
-    "name": "Fall Tree",
-    "portfolio": "",
-    "highlight": true
+    path: "ar-tree/style.css",
+    designer: "Aarav Patel",
+    name: "Fall Tree",
+    portfolio: "",
+    highlight: true,
   },
   {
-    "name": "The Cataclysm: as seen by scavenger in sewer",
-    "path": "pe-ter/style.css",
-    "designer": "Peter F. Parlee Carr",
-    "portfolio": ""
+    name: "The Cataclysm: as seen by scavenger in sewer",
+    path: "pe-ter/style.css",
+    designer: "Peter F. Parlee Carr",
+    portfolio: "",
   },
   {
-    "name": "Radio Music",
-    "path": "emily-music/style.css",
-    "designer": "Emily Myerov",
-    "portfolio": "",
-    "highlight": true
+    name: "Radio Music",
+    path: "emily-music/style.css",
+    designer: "Emily Myerov",
+    portfolio: "",
+    highlight: true,
   },
   {
-    "name": "Halo",
-    "path": "nathan-halo/style.css",
-    "designer": "Nathan E.",
-    "portfolio": "",
-    "highlight": true
+    name: "Halo",
+    path: "nathan-halo/style.css",
+    designer: "Nathan E.",
+    portfolio: "https://nathans-portfolio-project.netlify.app/",
+    highlight: true,
   },
   {
-    "name": "soccer",
-    "path": "emanuelly-ferreira/style.css",
-    "designer": "Emanuelly Ferreira.",
-    "portfolio": "your personal portfolio, if you have one — or claim a URL now on netlify and build on soon :)"
+    name: "soccer",
+    path: "emanuelly-ferreira/style.css",
+    designer: "Emanuelly Ferreira.",
+    portfolio:
+      "your personal portfolio, if you have one — or claim a URL now on netlify and build on soon :)",
   },
   {
-    "name": "Milky Way",
-    "path": "Will/style.css",
-    "designer": "Will K",
-    "portfolio": ""
+    name: "Milky Way",
+    path: "Will/style.css",
+    designer: "Will K",
+    portfolio: "",
   },
   {
-    "name": "your design name",
-    "path": "nai/style.css",
-    "designer": "Naima Kaldi",
-    "portfolio": "your personal portfolio, if you have one — or claim a URL now on netlify and build on soon :)"
+    name: "your design name",
+    path: "nai/style.css",
+    designer: "Naima Kaldi",
+    portfolio:
+      "your personal portfolio, if you have one — or claim a URL now on netlify and build on soon :)",
   },
   {
-    "name": "Simple Dark",
-    "path": "zander2023/style.css",
-    "designer": "Zander B-V",
-    "portfolio": "",
-    "highlight": true
+    name: "Simple Dark",
+    path: "zander2023/style.css",
+    designer: "Zander B-V",
+    portfolio: "",
+    highlight: true,
   },
   {
-    "name": "Tri-Tec Industries",
-    "path": "24-ethan-tritec/style.css",
-    "designer": "Ethan-Matte",
-    "portfolio": "https://sunryze-studios.netlify.app/web.html"
+    name: "Tri-Tec Industries",
+    path: "24-ethan-tritec/style.css",
+    designer: "Ethan-Matte",
+    portfolio: "https://sunryze-studios.netlify.app/web.html",
   },
   {
-    "name": "Simplicity",
-    "path": "24-ethan-simple/style.css",
-    "designer": "Ethan-Matte",
-    "portfolio": "https://sunryze-studios.netlify.app/web.html"
+    name: "Simplicity",
+    path: "24-ethan-simple/style.css",
+    designer: "Ethan-Matte",
+    portfolio: "https://sunryze-studios.netlify.app/web.html",
   },
   {
-    "name": "Retro Game",
-    "path": "24-ethan-retro/style.css",
-    "designer": "Ethan-matte",
-    "portfolio": "https://sunryze-studios.netlify.app/web.html",
-    "highlight": true
+    name: "Retro Game",
+    path: "24-ethan-retro/style.css",
+    designer: "Ethan-matte",
+    portfolio: "https://sunryze-studios.netlify.app/web.html",
+    highlight: true,
   },
   {
-    "name": "Zen Garden ",
-    "path": "24-luis-simple/style.css",
-    "designer": "Luis-Valiente",
-    "portfolio": "your personal portfolio, if you have one — or claim a URL now on netlify and build on soon :)"
+    name: "Zen Garden ",
+    path: "24-luis-simple/style.css",
+    designer: "Luis-Valiente",
+    portfolio:
+      "your personal portfolio, if you have one — or claim a URL now on netlify and build on soon :)",
   },
   {
-    "name": "Construction",
-    "path": "23-julian-construction/style.css",
-    "designer": "julian-henry",
-    "portfolio": "julianhenryportfolio.com",
-    "highlight": true
+    name: "Construction",
+    path: "23-julian-construction/style.css",
+    designer: "julian-henry",
+    portfolio: "https://remarkable-pithivier-572d14.netlify.app/",
+    highlight: true,
   },
   {
-    "name": "Cyberpunk",
-    "path": "23-julian-cyberpunk/style.css",
-    "designer": "julian-henry",
-    "portfolio": "julianhenryportfolio.com",
-    "highlight": true
+    name: "Cyberpunk",
+    path: "23-julian-cyberpunk/style.css",
+    designer: "julian-henry",
+    portfolio: "https://remarkable-pithivier-572d14.netlify.app/",
+    highlight: true,
   },
   {
-    "name": "Haunted Graveyard",
-    "path": "23-julian-haunted/style.css",
-    "designer": "julian-henry",
-    "portfolio": "julianhenryportfolio.com",
-    "highlight": true
+    name: "Haunted Graveyard",
+    path: "23-julian-haunted/style.css",
+    designer: "julian-henry",
+    portfolio: "https://remarkable-pithivier-572d14.netlify.app/",
+    highlight: true,
   },
   {
-    "name": "Soaring Sky",
-    "path": "seth-lupo-soaring-sky/style.css",
-    "designer": "Seth Lupo",
-    "portfolio": "",
-    "highlight": true
+    name: "Soaring Sky",
+    path: "seth-lupo-soaring-sky/style.css",
+    designer: "Seth Lupo",
+    portfolio: "https://sethlupo.dev/",
+    highlight: true,
   },
   {
-    "name": "Modernist Blocks",
-    "path": "seth-lupo-modernist-blocks/style.css",
-    "designer": "Seth Lupo",
-    "portfolio": "",
-    "highlight": true
+    name: "Modernist Blocks",
+    path: "seth-lupo-modernist-blocks/style.css",
+    designer: "Seth Lupo",
+    portfolio: "https://sethlupo.dev/",
+    highlight: true,
   },
   {
-    "name": "Limbo",
-    "path": "seth-lupo-limbo/style.css",
-    "designer": "Seth Lupo",
-    "portfolio": "",
-    "highlight": true
+    name: "Limbo",
+    path: "seth-lupo-limbo/style.css",
+    designer: "Seth Lupo",
+    portfolio: "https://sethlupo.dev/",
+    highlight: true,
   },
   {
-    "name": "Eli's Dark Mode",
-    "path": "eli-d/style.css",
-    "designer": "Elijah D",
-    "portfolio": ""
+    name: "Eli's Dark Mode",
+    path: "eli-d/style.css",
+    designer: "Elijah D",
+    portfolio: "",
   },
   {
-    "name": "Sky",
-    "path": "avery-sky/style.css",
-    "designer": "Avery K",
-    "portfolio": ""
+    name: "Sky",
+    path: "avery-sky/style.css",
+    designer: "Avery K",
+    portfolio: "",
   },
   {
-    "name": "Space",
-    "path": "avery-space/style.css",
-    "designer": "Avery K",
-    "portfolio": "",
-    "highlight": true
+    name: "Space",
+    path: "avery-space/style.css",
+    designer: "Avery K",
+    portfolio: "",
+    highlight: true,
   },
   {
-    "name": "The Theater",
-    "path": "avery-theater/style.css",
-    "designer": "Avery K",
-    "portfolio": "",
-    "highlight": true
+    name: "The Theater",
+    path: "avery-theater/style.css",
+    designer: "Avery K",
+    portfolio: "",
+    highlight: true,
   },
   {
-    "name": "Bold Lines",
-    "path": "owenk/style.css",
-    "designer": "Owen K",
-    "portfolio": "",
-    "highlight": true
+    name: "Bold Lines",
+    path: "owenk/style.css",
+    designer: "Owen K",
+    portfolio: "",
+    highlight: true,
   },
   {
-    "name": "Space Wide",
-    "path": "lex-space/style.css",
-    "designer": "Lex R",
-    "portfolio": ""
-  }
+    name: "Space Wide",
+    path: "lex-space/style.css",
+    designer: "Lex R",
+    portfolio: "",
+  },
 ];
 exports.styles = styles;
